78722a11f5905c88407edf0b7e09340801d48df6
harrisnic <harris.nicolaou@gmail.com>
2021-05-19 13:06:30 +0300

    WIP Transaction controller

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\TransactionResource;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use App\Models\Transaction;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Symfony\Component\HttpFoundation\Response;
+
+class TransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return AnonymousResourceCollection
+     */
+    public function index(): AnonymousResourceCollection
+    {
+        $transactions = Transaction::all();
+        return TransactionResource::collection($transactions);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return TransactionResource
+     */
+    public function store(): TransactionResource
+    {
+        $data = $this->validateData();
+        $transaction = Transaction::create($data);
+
+        return new TransactionResource($transaction);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Transaction $transaction
+     * @return TransactionResource
+     */
+    public function show(Transaction $transaction): TransactionResource
+    {
+        return new TransactionResource($transaction);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Transaction $transaction
+     * @return TransactionResource
+     */
+    public function update(Transaction $transaction): TransactionResource
+    {
+        $data = $this->validateData();
+        $transaction->update($data);
+        return new TransactionResource($transaction);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Transaction $transaction
+     * @return JsonResponse
+     */
+    public function destroy(Transaction $transaction): JsonResponse
+    {
+        $transaction->delete();
+        return response()->json('Transaction deleted', Response::HTTP_OK);
+    }
+
+    private function validateData(): array
+    {
+        return request()->validate([
+            'amount' => ['required', 'numeric']
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/transactions', [TransactionController::class, 'index']);
+Route::post('/transactions', [TransactionController::class, 'store']);
+Route::get('/transactions/{transaction}', [TransactionController::class, 'show']);
+Route::put('/transactions/{transaction}', [TransactionController::class, 'update']);
+Route::delete('/transactions/{transaction}', [TransactionController::class, 'destroy']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
